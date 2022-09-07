59a8c405069636070b7d63377f2f7bf180cf8941
matthias <you@example.com>
2022-09-07 16:52:55 +0200

    add project

---
+++ hero-project/projects/ironman/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ironman
+ */
+
+export * from '@ironman/src/lib/feature-a';
+export * from '@ironman/src/lib/feature-b';
+
---
+++ hero-project/projects/libs/shared/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of shared
+ */
+
+export * from '@libs/shared/src/lib/feature-a';
+export * from '@libs/shared/src/lib/feature-b';
+
